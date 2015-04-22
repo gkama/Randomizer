dad6d233bc4607ba990e719ef79d9c931b6240c3
gogyprogramming <gkamacharov@gmail.com>
2015-04-22 18:17:15 -0400

    Randomizer

---
+++ DigitTranslator.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Randomizer
+{
+	public class DigitTranslator
+	{
+		static char[] allLettersArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+		static char[] zeroLettersArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+		static char[] twoLettersArray = "ABC".ToCharArray();
+		static char[] threeLettersArray = "DEF".ToCharArray();
+		static char[] fourLettersArray = "GHI".ToCharArray();
+		static char[] fiveLettersArray = "JKL".ToCharArray();
+		static char[] sixLettersArray = "MNO".ToCharArray();
+		static char[] sevenLettersArray = "PQRS".ToCharArray();
+		static char[] eightLettersArray = "TUV".ToCharArray();
+		static char[] nineLettersArray = "WXYZ".ToCharArray();
+
+		public DigitTranslator ()
+		{
+		}
+
+		// Checking correct input
+		public static bool isDigit(string s){
+			bool errorCounter = Regex.IsMatch(s, @"[^0-9]");
+			if (errorCounter == true) 
+			{
+				return false;
+			}
+			return true;
+		}
+
+		public static string toRandomString (string s)
+		{
+			return s.ToString ();
+		}
+
+
+		// Function to get random Numbers
+		private static readonly Random randomNum = new Random();
+		private static readonly object syncLock = new object();
+		public static int GetRandomNumber(int min, int max)
+		{
+			lock(syncLock) { // synchronize
+				return randomNum .Next(min, max);
+			}
+		}
+
+
+		// Randomize algorithm
+		public static string Randomize(string s)
+		{
+			StringBuilder result = new StringBuilder();
+			for (int i = 0; i < s.Length; i++) {
+				if (s[i] == '0') {
+					result.Append (allLettersArray[GetRandomNumber (0, 26)].ToString ());
+				} else if (s[i] == '1') {
+					result.Append (allLettersArray[GetRandomNumber (0, 26)].ToString ());
+				} else if (s[i] == '2') {
+					result.Append (twoLettersArray[GetRandomNumber (0, 3)].ToString ());
+				} else if (s[i] == '3') {
+					result.Append (threeLettersArray[GetRandomNumber (0, 3)].ToString ());
+				} else if (s[i] == '4') {
+					result.Append (fourLettersArray[GetRandomNumber (0, 3)].ToString ());
+				} else if (s[i] == '5') {
+					result.Append (fiveLettersArray[GetRandomNumber (0, 3)].ToString ());
+				} else if (s[i] == '6') {
+					result.Append (sixLettersArray[GetRandomNumber (0, 3)].ToString ());
+				} else if (s[i] == '7') {
+					result.Append (sevenLettersArray[GetRandomNumber (0, 4)].ToString ());
+				} else if (s[i] == '8') {
+					result.Append (eightLettersArray[GetRandomNumber (0, 3)].ToString ());
+				} else if (s[i] == '9') {
+					result.Append (nineLettersArray[GetRandomNumber (0, 4)].ToString ());
+				}
+			}
+
+			return result.ToString();
+		}
+			
+	}
+}
+
---
+++ MainActivity.cs
@@ -0,0 +1,110 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Randomizer
+{
+	[Activity (Label = "Randomizer", MainLauncher = true, LaunchMode = Android.Content.PM.LaunchMode.SingleInstance, ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]			
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView(Resource.Layout.MainScreen);
+
+			string category;
+
+			// Buttons
+			Button fourdigitsCategory = FindViewById<Button> (Resource.Id.fourdigitsCategory);
+			Button fivedigitsCategory = FindViewById<Button> (Resource.Id.fivedigitsCategory);
+			Button sixdigitsCategory = FindViewById<Button> (Resource.Id.sixdigitsCategory);
+			Button sevendigitsCategory = FindViewById<Button> (Resource.Id.sevendigitsCategory);
+			Button tendigitsCategory = FindViewById<Button> (Resource.Id.tendigitsCategory);
+			Button fifteendigitsCategory = FindViewById<Button> (Resource.Id.fifteendigitsCategory);
+			Button twentyfivedigitsCategory = FindViewById<Button> (Resource.Id.twentyfivedigitsCategory);
+
+			var titleText = FindViewById<TextView> (Resource.Id.titleText);
+			var chooseaCategoryText = FindViewById<TextView> (Resource.Id.chooseaCategoryText);
+			var authorText = FindViewById<TextView> (Resource.Id.authorText);
+
+			fourdigitsCategory.TextSize = 15;
+			fivedigitsCategory.TextSize = 15;
+			sixdigitsCategory.TextSize = 15;
+			sevendigitsCategory.TextSize = 15;
+			tendigitsCategory.TextSize = 15;
+			fifteendigitsCategory.TextSize = 15;
+			twentyfivedigitsCategory.TextSize = 15;
+
+			titleText.TextSize = 40;
+			chooseaCategoryText.TextSize = 20;
+			authorText.TextSize = 10;
+
+			fourdigitsCategory.Click += delegate {
+				category = "4";
+				Intent slideIntent = new Intent(this, typeof(RandomizeActivity));
+				slideIntent.PutExtra("category", category);
+				Bundle slideAnim = ActivityOptions.MakeCustomAnimation(Application.Context, Resource.Animation.Anim1, Resource.Animation.Anim2).ToBundle();
+				StartActivity(slideIntent, slideAnim);
+			};
+
+			fivedigitsCategory.Click += delegate {
+				category = "5";
+				Intent slideIntent = new Intent(this, typeof(RandomizeActivity));
+				slideIntent.PutExtra("category", category);
+				Bundle slideAnim = ActivityOptions.MakeCustomAnimation(Application.Context, Resource.Animation.Anim1, Resource.Animation.Anim2).ToBundle();
+				StartActivity(slideIntent, slideAnim);
+			};
+
+			sixdigitsCategory.Click += delegate {
+				category = "6";
+				Intent slideIntent = new Intent(this, typeof(RandomizeActivity));
+				slideIntent.PutExtra("category", category);
+				Bundle slideAnim = ActivityOptions.MakeCustomAnimation(Application.Context, Resource.Animation.Anim1, Resource.Animation.Anim2).ToBundle();
+				StartActivity(slideIntent, slideAnim);
+			};
+
+			sevendigitsCategory.Click += delegate {
+				category = "7";
+				Intent slideIntent = new Intent(this, typeof(RandomizeActivity));
+				slideIntent.PutExtra("category", category);
+				Bundle slideAnim = ActivityOptions.MakeCustomAnimation(Application.Context, Resource.Animation.Anim1, Resource.Animation.Anim2).ToBundle();
+				StartActivity(slideIntent, slideAnim);
+			};
+
+			tendigitsCategory.Click += delegate {
+				category = "10";
+				Intent slideIntent = new Intent(this, typeof(RandomizeActivity));
+				slideIntent.PutExtra("category", category);
+				Bundle slideAnim = ActivityOptions.MakeCustomAnimation(Application.Context, Resource.Animation.Anim1, Resource.Animation.Anim2).ToBundle();
+				StartActivity(slideIntent, slideAnim);
+			};
+
+			fifteendigitsCategory.Click += delegate {
+				category = "15";
+				Intent slideIntent = new Intent(this, typeof(RandomizeActivity));
+				slideIntent.PutExtra("category", category);
+				Bundle slideAnim = ActivityOptions.MakeCustomAnimation(Application.Context, Resource.Animation.Anim1, Resource.Animation.Anim2).ToBundle();
+				StartActivity(slideIntent, slideAnim);
+			};
+
+			twentyfivedigitsCategory.Click += delegate {
+				category = "25";
+				Intent slideIntent = new Intent(this, typeof(RandomizeActivity));
+				slideIntent.PutExtra("category", category);
+				Bundle slideAnim = ActivityOptions.MakeCustomAnimation(Application.Context, Resource.Animation.Anim1, Resource.Animation.Anim2).ToBundle();
+				StartActivity(slideIntent, slideAnim);
+			};
+		}
+	}
+}
+
